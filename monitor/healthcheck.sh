7121c415d43c83ce82503245aba811282ee6fdb6
Scoutfarsan <191983807+Scoutfarsan@users.noreply.github.com>
2025-09-10 15:03:34 +0200

    Delete monitor directory

--- monitor/healthcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-dig +time=2 +tries=1 pi-hole.net @127.0.0.1 -p 53 >/dev/null
-systemctl is-active --quiet pihole-FTL
-systemctl is-active --quiet unbound
-systemctl is-active --quiet lighttpd
-systemctl is-active --quiet wg-quick@wg0 || true
-echo "OK"
