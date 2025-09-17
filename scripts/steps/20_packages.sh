85fd34e2166f91987833a0d346cf6eb4e021f32a
Scoutfarsan <191983807+Scoutfarsan@users.noreply.github.com>
2025-09-17 08:12:26 +0200

    Delete scripts directory

--- scripts/steps/20_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
-. "$ROOT_DIR/scripts/lib/env.sh"
-
-log "[20] paket"
-sudo apt-get update -y
-sudo apt-get install -y lighttpd unbound dnsutils qrencode git
-log "[20] klar"
