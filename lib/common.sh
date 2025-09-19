54c7878cad46c9c9c753d62cc3ed193afcafe614
Scoutfarsan <191983807+Scoutfarsan@users.noreply.github.com>
2025-09-19 16:33:10 +0200

    Update common.sh

--- lib/common.sh
+++ lib/common.sh
@@ -1,1 +1,10 @@
 # load_env stub
+
+# QoL aliases för screen
+if ! grep -q "alias scr=" /etc/profile.d/gandalf-aliases.sh 2>/dev/null; then
+  cat >/etc/profile.d/gandalf-aliases.sh <<'AL'
+alias scr='screen -DR'
+alias scls='screen -ls'
+AL
+  chmod 0644 /etc/profile.d/gandalf-aliases.sh
+fi
